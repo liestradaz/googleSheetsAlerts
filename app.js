1b98132a40c5fc44a940cd83f296d724fd59bf5e
nash0x <nachoest@gmail.com>
2024-02-27 10:23:21 -0700

    Adding all files

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+var express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+
+var app = express();
+const port = 3000;
+
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(botToken);
+
+app.use(bodyParser.json());
+
+app.post('/supertrendAlerts', (req, res) => {
+    // Handle webhook from TradingView
+    //const message = req.body.message;
+    const message = 'hol';
+
+    // Send message to Telegram
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+
+    res.status(200).send('Message sent to Telegram');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

9562361b83c09f06669d0e924c8f5ba84c718f38
nash0x <nachoest@gmail.com>
2024-02-29 10:14:00 -0700

    Added logging via Morgan

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 var express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 
@@ -9,11 +10,15 @@ const port = 3000;
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(botToken);
 
+// Setup morgan middleware for logging
+app.use(morgan('combined'));
+
 app.use(bodyParser.json());
 
 app.post('/supertrendAlerts', (req, res) => {
     // Handle webhook from TradingView
     const message = req.body.message;
+    console.log('Message: ', message)
 
     // Send message to Telegram
     bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);

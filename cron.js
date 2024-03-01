32644b3b18f678d41718a5c1fda6e4e3383e73b4
nash0x <nachoest@gmail.com>
2024-03-01 11:58:47 -0700

    Fixed URL bug

--- cron.js
+++ cron.js
@@ -2,8 +2,9 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 // Schedule a task to send an HTTP request every 10 minutes
-cron.schedule('*/1 * * * *', () => {
-    axios.get(process.env.APP_URL || 'http://localhost:3000' + '/health')
+cron.schedule('*/14 * * * *', () => {
+    const URL = process.env.APP_URL || 'http://localhost:3000'
+    axios.get( URL + '/health')
         .then(response => {
             console.log('Ping sent successfully');
         })
